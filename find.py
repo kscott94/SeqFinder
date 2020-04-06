35588328b7fcfd1578dd6f8075a821853bd2245a
Kristin Scott <50966226+kscott94@users.noreply.github.com>
2020-04-05 19:52:44 -0600

    Update find.py

--- find.py
+++ find.py
@@ -1,18 +1,31 @@
 #!/usr/bin/env python3
 
 import argparse
-import os
 
 parser = argparse.ArgumentParser()
-parser.add_argument('txt', type=str)    #path/genome.txt
+
+parser.add_argument('genome', type=str)    #path/genome.fa
 parser.add_argument('start', type=int)
-parser.add_argument('stop', type=int)
+parser.add_argument('stop', type=int )
+parser.add_argument('-fa','--fasta', action="store_true", help="output in fasta format")
+
 
 args = parser.parse_args()
 
 
-with open(args.txt, 'r') as genome:
-    pos = genome.read().replace('\n', '')
-    args.start = args.start - 1
-    genome_seq = pos[args.start:args.stop]
-    print(genome_seq)
+with open(args.genome, 'r') as genome:
+    """remove fasta header"""
+    genome_sequence_initial = genome.readlines()[1:]
+    genome_sequence = ''.join(genome_sequence_initial)
+
+    """correct for 0 based indexing"""
+    args.start += - 1
+
+    """create and print sequence object"""
+    subseted_sequence = genome_sequence[args.start:args.stop]
+
+    if args.fasta:
+        print('>positions %i-%i\n%s' % (args.start, args.stop, subseted_sequence))
+    else:
+        print(subseted_sequence)
+
