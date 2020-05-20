306a54ce82c0cd0f8858a16c21e4a7b03b09ab59
Kristin Scott <50966226+kscott94@users.noreply.github.com>
2020-05-20 17:15:04 -0600

    Update find.py

--- find.py
+++ find.py
@@ -3,17 +3,14 @@
 import argparse
 
 parser = argparse.ArgumentParser()
-
 parser.add_argument('genome', type=str)    #path/genome.fa
 parser.add_argument('start', type=int)
 parser.add_argument('stop', type=int )
 parser.add_argument('-fa','--fasta', action="store_true", help="output in fasta format")
 
-
 args = parser.parse_args()
 
-
-with open(args.genome, 'r') as genome:
+with open(args.genome, 'r') as genome:      # Read genome fasta file and pull out sequence of interest
     """remove fasta header"""
     genome_sequence_initial = genome.readlines()[1:]
     genome_sequence = ''.join(genome_sequence_initial).replace("\n","")
