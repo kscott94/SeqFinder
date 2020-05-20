1d31b8d588129c36670686afb5e0ff5102eae233
Kristin Scott <50966226+kscott94@users.noreply.github.com>
2020-05-20 17:12:46 -0600

    Update fasta_pull.py

--- fasta_pull.py
+++ fasta_pull.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 from Bio import SeqIO
 
 list_of_genes = "./TS559_m5c_transcripts"
@@ -8,13 +6,13 @@ fasta_file = "./data/tk_genes.fa"
 fasta_dict = {rec.id : rec.seq for rec in SeqIO.parse(fasta_file, "fasta")}
 subset_dict = {}
 
-with open(list_of_genes,"r") as genes:
+with open(list_of_genes,"r") as genes:  # Pull fasta sequence of genes from master file
     for gene in genes:
         gene=gene.strip()
-        gene_sequence = str(fasta_dict[gene]).upper().replace("T", "U")
+        gene_sequence = str(fasta_dict[gene]).upper().replace("T", "U") # Direct conversion of DNA coding sequence to RNA
         subset_dict.update({gene : gene_sequence})
 
-with open("./data/fasta_subset.fa", "a") as subsetted_fasta:
+with open("./data/fasta_subset.fa", "a") as subsetted_fasta:    # Creat fasta file of subsetted dictionary
     for key in subset_dict:
         subsetted_fasta.write(">%s\n" % key)
         subsetted_fasta.write(subset_dict[key])
