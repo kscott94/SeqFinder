801f89d25c76dc115011a46d64209400d2bcd24d
Kristin Scott <50966226+kscott94@users.noreply.github.com>
2020-12-26 22:39:40 -0700

    Update makebedfile.py

--- makebedfile.py
+++ makebedfile.py
@@ -5,13 +5,14 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('start', type=int)
 parser.add_argument('end', type=int)
-parser.add_argument('--name', type=str, default='chr')
+parser.add_argument('--name', type=str, default='chromosome1')
 parser.add_argument('--size', type=int, default=100)
 parser.add_argument('--step', type=int, default=100)
 args = parser.parse_args()
 
-c = args.start
+s = args.start
+e = args.end
 
-while c < args.end:
-    print('%s\t%i\t%i' % (args.name, c, c + args.size))
-    c = c + args.step
+while s < e:
+    print('%s\t%i\t%i' % (args.name, s, s + args.size))
+    s = s + args.step
